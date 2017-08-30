0e9b4fb6f416d26b946e7961ba575402e09ee51e
Justin Daniel <justinddaniel@gmail.com>
2017-08-30 19:17:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const rl = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   for (var i = 0; i < rl.length; i++) {
-    rl[i].innerHTML = parseInt(rl[i], 10) + n
+    rl[i].innerHTML = parseInt(`${rl[i]}`, 10) + n
   } return rl
 }
