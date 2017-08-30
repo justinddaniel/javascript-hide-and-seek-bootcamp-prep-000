c21a222619123c37b4e02712fa96935f028796ad
Justin Daniel <justinddaniel@gmail.com>
2017-08-30 21:25:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var rl = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < rl.length; i++) {
+    rl[i].innerHTML = parseInt(rl[i].innerHTML, 10) + n
+  } return rl
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
