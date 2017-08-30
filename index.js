e3db8bef4592e76e1a876f9c7a1419cd49bdedb1
Justin Daniel <justinddaniel@gmail.com>
2017-08-30 19:20:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function increaseRankBy(n) {
   const rl = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   for (var i = 0; i < rl.length; i++) {
     rl[i].innerHTML = parseInt(`${rl[i]}`, 10) + n
-  } return rl.innerHTML
+  } 
 }
